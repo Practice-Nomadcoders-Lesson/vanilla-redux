c949e44f72c58d756d32bb488163b093fd40ffa8
iinsue <hinsu93@naver.com>
2022-04-09 15:14:18 +0900

    #3.2 mapStateToProps

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 
-function Home() {
+function Home({ toDos }) {
   const [text, setText] = useState("");
   function onChange(event) {
     setText(event.target.value);
@@ -18,9 +19,13 @@ function Home() {
         <input onChange={onChange} type="text" value={text} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
 
-export default Home;
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+export default connect(mapStateToProps)(Home);
